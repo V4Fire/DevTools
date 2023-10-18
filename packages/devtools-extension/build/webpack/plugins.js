abc11fd4901523c1cfda2637b50f49877fffd230
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-10-19 00:13:39 +0700

    chore(devtools-extension): init webpack plugins

---
+++ packages/devtools-extension/build/webpack/plugins.js
@@ -0,0 +1,36 @@
+/*!
+ * V4Fire DevTools
+ * https://github.com/V4Fire/DevTools
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/DevTools/blob/main/LICENSE
+ */
+'use strict';
+
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const
+	parent = include('@super/build/webpack/plugins', __dirname),
+	ExtensionManifestPlugin = require('./plugins/extension-manifest');
+
+/**
+ * Init webpack plugins
+ *
+ * @param {object} root0
+ * @returns {Map}
+ */
+module.exports = async function plugins({buildId, name}) {
+	const plugins = new Map(await parent({buildId, name}));
+
+	plugins.set('copy-icons', new CopyPlugin({
+		patterns: [{from: 'src/assets/icons', to: 'assets/icons'}]
+	}));
+
+	plugins.set('extension-manifest-plugin', new ExtensionManifestPlugin({
+		manifestTemplatePath: path.resolve('manifest.template.json'),
+		manifestV2TemplatePath: path.resolve('manifest.v2.template.json')
+	}));
+
+	return plugins;
+};
