e256ed8139212ac12f5ccfb361ae3f3135cc5d1c
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-10-20 19:17:03 +0700

    feat(b-header): add header widget

---
+++ packages/devtools-core/src/widgets/b-header/index.js
@@ -0,0 +1,15 @@
+/*!
+ * V4Fire DevTools
+ * https://github.com/V4Fire/DevTools
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/DevTools/blob/main/LICENSE
+ */
+
+'use strict';
+
+package('b-header')
+	.extends('i-block')
+	.dependencies(
+		'b-button'
+	)
