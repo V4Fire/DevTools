5d0c7bfa286a5a218021319ecdc9d1d81651d98d
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-10-31 19:41:49 +0700

    feat(p-components): add panel for component meta

--- packages/devtools-core/src/pages/p-components/p-components.ss
+++ packages/devtools-core/src/pages/p-components/p-components.ss
@@ -4,10 +4,38 @@
 
 - template index() extends ['i-dynamic-page'].index
 	- block body
-		< b-tree &
-			:items = tree |
-			:folded = false |
-			:theme = 'demo'
-		.
-			< template #default = {item}
-       {{ item.label }} value={{ item.value }}
+		< .&__content
+			< b-tree &
+				ref = tree |
+				:items = components |
+				:folded = false |
+				:theme = 'demo' |
+				:cancelable = true
+			.
+				< template #default = {item}
+					{{ item.label }} value={{ item.value }}
+
+			< template v-if = selectedComponentId
+				< template v-if = selectedComponentMeta.length > 0
+					< div
+						< bold
+							{{ selectedComponentName }}
+						< b-tree &
+							ref = panel |
+							:items = selectedComponentMeta |
+							:theme = 'demo' |
+							:cancelable = true
+						.
+							< template #default = {item}
+								{{ item.label }}
+								< template v-if = item.data !== undefined
+									= {{ item.data }}
+				< p v-else
+					< i
+						Loading...
+
+			< p v-else
+				< i
+					Select a component
+
+
--- packages/devtools-core/src/pages/p-components/p-components.ts
+++ packages/devtools-core/src/pages/p-components/p-components.ts
@@ -6,18 +6,74 @@
  * https://github.com/V4Fire/DevTools/blob/main/LICENSE
  */
 
-import iDynamicPage, { component, field } from 'components/super/i-dynamic-page/i-dynamic-page';
+import symbolGenerator from 'core/symbol';
+import iDynamicPage, { component, field, computed, watch } from 'components/super/i-dynamic-page/i-dynamic-page';
 
+import type bTree from '@v4fire/client/components/base/b-tree/b-tree';
 import type { Item } from 'components/base/b-tree/b-tree';
 
 export * from 'components/super/i-dynamic-page/i-dynamic-page';
 
+const $$ = symbolGenerator();
+
 @component()
 export default class pComponents extends iDynamicPage {
+	override readonly $refs!: iDynamicPage['$refs'] & {
+		tree?: bTree;
+		panel?: bTree;
+	};
 
 	/**
 	 * Component tree
 	 */
 	@field()
-	tree: Item[] = [];
+	components: Item[] = [];
+
+	/**
+	 * Selected component id
+	 */
+	@field()
+	selectedComponentId: string | null = null;
+
+	/**
+	 * Selected component meta
+	 */
+	@field()
+	selectedComponentMeta: Item[] = [];
+
+	@computed()
+	get selectedComponentName(): string | null {
+		return <string>this.$refs.tree?.getItemByValue(this.selectedComponentId)?.componentName;
+	}
+
+	/**
+	 * Handle component select
+	 *
+	 * @param _
+	 * @param componentId
+	 */
+	@watch('?$refs.tree:change')
+	onComponentSelect(_: unknown, componentId: string): void {
+		this.selectedComponentId = componentId;
+		this.selectedComponentMeta = [];
+
+		const load = this.async.debounce(async () => {
+			try {
+				await this.loadSelectedComponentMeta();
+			} catch (error) {
+				// TODO: show alert
+				stderr(error);
+			}
+		}, 300, {label: $$.loadSelectedComponentMeta});
+
+		load();
+	}
+
+	/**
+	 * Loads selected component meta
+	 */
+	loadSelectedComponentMeta(): CanPromise<void> {
+		// Override
+	}
+
 }
