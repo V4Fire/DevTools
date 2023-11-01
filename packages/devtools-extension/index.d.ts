f2baf43d2355870ee1d3ed9138fedbfceb7079dc
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-11-01 19:59:01 +0700

    chore(devtools-extension): add devtools-backend to deps

--- packages/devtools-extension/index.d.ts
+++ packages/devtools-extension/index.d.ts
@@ -6,7 +6,14 @@
  * https://github.com/V4Fire/DevTools/blob/main/LICENSE
  */
 
+/* eslint-disable no-var */
+
 /// <reference types="@v4fire/devtools-core"/>
 
 declare const
 	browser: typeof chrome;
+
+/**
+ * Devtools backend API - available only in the inspected window context
+ */
+declare var __V4FIRE_DEVTOOLS_BACKEND__: typeof import('@v4fire/devtools-backend');
---
+++ packages/devtools-extension/src/backend.ts
@@ -0,0 +1,11 @@
+/*!
+ * V4Fire DevTools
+ * https://github.com/V4Fire/DevTools
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/DevTools/blob/main/LICENSE
+ */
+
+import * as api from '@v4fire/devtools-backend';
+
+globalThis.__V4FIRE_DEVTOOLS_BACKEND__ = api;
