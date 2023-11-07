56e51161426c017c72f77dc79f91964f13d24ec0
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-11-07 19:12:57 +0700

    chore(devtools-extension): export @super/shared/lib

--- packages/devtools-extension/src/shared/lib/index.ts
+++ packages/devtools-extension/src/shared/lib/index.ts
@@ -6,5 +6,6 @@
  * https://github.com/V4Fire/DevTools/blob/main/LICENSE
  */
 
+export * from '@super/shared/lib';
 export * from 'shared/lib/browser-api';
 export { default as pageHasV4Fire } from 'shared/lib/page-has-v4fire';
