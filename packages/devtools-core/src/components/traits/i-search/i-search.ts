717762cc16f2608ed85fa9dffb393638bd068619
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-11-17 16:44:48 +0700

    docs(i-search): add jsdoc for events

--- packages/devtools-core/src/components/traits/i-search/i-search.ts
+++ packages/devtools-core/src/components/traits/i-search/i-search.ts
@@ -14,9 +14,32 @@ interface iSearch extends iBlock {}
 
 abstract class iSearch<Item = unknown> {
 	readonly LocalEmitter!: InferEvents<[
+		/**
+		 * Highlight event for specific node
+		 *
+		 * @param indices
+		 * @example
+		 * ```
+		 * const id = 1;
+		 * const startIndex = 'text to highlight'.indexOf('to high');
+		 * const stopIndex = startIndex + 'to high'.length;
+		 * emitter.emit(`highlight.${id}`, [startIndex, stopIndex]);
+		 * ```
+		 */
 		[`highlight.${string}`, [number, number] | null],
+		/**
+		 * Mark highlighted node as selected
+		 *
+		 * @param selected
+		 */
 		[`highlight-current.${string}`, boolean],
+		/**
+		 * Resets highlight on all elements for given ctx
+		 */
 		['highlight-reset'],
+		/**
+		 * General highlight event, which will be emitted to all nodes with v-highlight
+		 */
 		['highlight']
 	], iBlock['LocalEmitter']>;
 
