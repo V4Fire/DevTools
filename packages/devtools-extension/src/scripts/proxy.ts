5b9448b66dbef38e650e868c41295b48a509315d
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2023-12-12 20:17:09 +0700

    feat(scripts/proxy): connect to extension's service worker

--- packages/devtools-extension/src/scripts/proxy.ts
+++ packages/devtools-extension/src/scripts/proxy.ts
@@ -10,8 +10,14 @@
 // @see: https://chromium.googlesource.com/chromium/src/+/master/third_party/blink/renderer/bindings/core/v8/V8BindingDesign.md#world
 // It can forward messages from content scripts running in MAIN world to the extension's service worker.
 
+let port: chrome.runtime.Port | null = null;
+
 globalThis.addEventListener('pageshow', () => {
 	globalThis.addEventListener('message', handleMessageFromPage);
+
+	if (port == null) {
+		connectPort();
+	}
 });
 
 globalThis.addEventListener('pagehide', () => {
@@ -25,6 +31,12 @@ function handleMessageFromPage(event: MessageEvent) {
 	}
 
 	switch (event.data.source) {
+		// This is a message from a bridge (initialized by a devtools backend)
+		case 'v4fire-devtools-bridge':
+			port?.postMessage(event.data.payload);
+			break;
+
+		// This is a message from a detect script
 		case 'v4fire-devtools-detect': {
 			const {source, payload} = event.data;
 			void chrome.runtime.sendMessage({source, payload});
@@ -35,4 +47,32 @@ function handleMessageFromPage(event: MessageEvent) {
 		default:
 			// Do nothing
 	}
+}
+
+function handleMessageFromDevtools(message: any) {
+	globalThis.postMessage(
+		{
+			source: 'v4fire-devtools-content-script',
+			payload: message
+		},
+		'*'
+	);
+}
+
+function handleDisconnect() {
+	port = null;
+
+	// Try to reconnect
+	connectPort();
+}
+
+// Creates port from application page to the V4Fire DevTools' service worker
+// Which then connects it with devtools port
+function connectPort() {
+	port = chrome.runtime.connect({
+		name: 'proxy'
+	});
+
+	port.onMessage.addListener(handleMessageFromDevtools);
+	port.onDisconnect.addListener(handleDisconnect);
 }
